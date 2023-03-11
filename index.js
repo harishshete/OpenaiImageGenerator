0e58975b250f4de91b64321b969bdaf640d72fb1
harishshete <harishshete430@gmail.com>
2023-03-12 00:41:27 +0530

    update 3

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 const express = require("express");
 const dotenv = require("dotenv").config({ path: './config/.env'});
 const routes =  require("./routes/openaiRoutes");
+var logger = require('morgan');
 
 const PORT = process.env.PORT || 5000;
 
 const app = express();
 
 app.set("view engine", "ejs");
+app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
